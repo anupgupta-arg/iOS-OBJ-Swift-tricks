7ffcca351abb6a00de9d11f9c31fc9438d39e348
anup gupta <im.anupgupta03@gmail.com>
2018-02-06 12:10:14 +0530

    NSDictionary -> JSON String Swift

--- NSDictionary+JsonString.swift
+++ NSDictionary+JsonString.swift
@@ -1,3 +1,12 @@
+//
+//  NSDictionary+JsonDictionary.h
+//  GeekGuns
+//
+//  Created by GeekGuns Developer on 31/01/18.
+//  Copyright © 2018 GeekGuns. All rights reserved.
+//  Free to use
+
+
 import Foundation
 import Swift
 
